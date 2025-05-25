b3867d1b62e13cf9a42739f21c2f206fa074f9a2
Skarim <Sajidkarimbilali9@gmail.com>
2025-05-25 17:31:19 +0500

    new modification is added like i add switch based menu

--- ThomasTravels/src/com/TestDriver.java
+++ ThomasTravels/src/com/TestDriver.java
@@ -8,18 +8,27 @@ public class TestDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		String category = input.next().toLowerCase();
+		Scanner scanner = new Scanner(System.in);
+		// String category = input.next().toLowerCase();
+		System.out.println("Add new Entry Of Driver Press 1");
+		System.out.println("Find Driver By Id Press 2");
+		System.out.println("Find Number Of Drivers By Catagory Press 3");
+		System.out.println("Find Perticuler Driver By Catagory Press 4");
+		System.out.println("Find maximum distance covered Press 5");
+
+		int choice = input.nextInt();
+
 		Boolean isValid = false;
 
-		switch (category) {
-		case "car":
-		case "lorry":
-		case "auto":
-			isValid = true;
-			break;
-		default:
-			isValid = false;
-		}
+//		switch (category) {
+//		case "car":
+//		case "lorry":
+//		case "auto":
+//			isValid = true;
+//			break;
+//		default:
+//			isValid = false;
+//		}
 
 		Travel objTravel = new Travel();
 		// Validate driver
@@ -31,24 +40,84 @@ public class TestDriver {
 		drivers.add(new Driver("car", 3, "Nitesh", 45000));
 		drivers.add(new Driver("lorry", 4, "Ustaad", 42000));
 
-		// Getting searched Drivers
-		String message = Travel.RetrivebyDriverId(drivers, 2);
-		System.out.println(message);
+		// Adding new Recod
+		switch (choice) {
+		case 1:
+			while (true) {
+				// Scanner scanner = new Scanner(System.in);
+				System.out.println("Enter Drivers Catagory.");
 
-		// Getting count of Searched Number of list
-		int noOfDrivers = Travel.RetriveCountOfDriver(drivers, category);
-		System.out.println("count of drivers. " + noOfDrivers);
+				String catagory = scanner.nextLine();
 
-		// Getting List of searched Category
-		ArrayList<Driver> noOfDriversList = Travel.retriveDriver(drivers, category);
-		for (Driver matchingDrivers : noOfDriversList) {
-			System.out.println(matchingDrivers.getDriverName() + " - " + matchingDrivers.getCatagory());
+				System.out.println("Enter Drivers Id.");
+
+				Integer driverId = scanner.nextInt();
+
+				System.out.println("Enter Drivers Name.");
+				scanner.nextLine();
+				String driverName = scanner.nextLine();
+
+				System.out.println("Enter Drivers Total Distance.");
+				Integer totaldistance = scanner.nextInt();
+				scanner.nextLine();
+				drivers.add(new Driver(catagory, driverId, driverName, totaldistance));
+				for (Driver driver : drivers) {
+					System.out.println(" " + driver);
+				}
+				System.out.println("If you want to exit then Type 'done' .");
+				String exit = scanner.nextLine();
+				if (exit.equalsIgnoreCase("done")) {
+					break;
+				}
+
+			}
+			break;
+		case 2:
+
+			System.out.println("Enter Drivers Id.");
+
+			Integer driverId = scanner.nextInt();
+			// Getting searched Drivers
+			String message = Travel.RetrivebyDriverId(drivers, driverId);
+			System.out.println(message);
+			System.out.println("Go Back Menu");
+
+		case 3:
+
+			System.out.println("Enter Drivers Catagory.");
+
+			String catagory = scanner.nextLine();
+
+			// Getting count of Searched Number of list
+			int noOfDrivers = Travel.RetriveCountOfDriver(drivers, catagory);
+			System.out.println("count of drivers. " + noOfDrivers);
+			break;
+
+		case 4:
+
+			System.out.println("Enter Drivers Catagory.");
+
+			String searchedBycatagory = scanner.nextLine();
+
+			// Getting List of searched Category
+			ArrayList<Driver> noOfDriversList = Travel.retriveDriver(drivers, searchedBycatagory);
+			for (Driver matchingDrivers : noOfDriversList) {
+				System.out.println(matchingDrivers.getDriverName() + " - " + matchingDrivers.getCatagory());
+			}
+			break;
+
+		case 5:
+
+			// Getting MaximumDisttance TravelledDriver
+			int maximumTravelledList = Travel.RetriveMaximumDistanceTravelledDriver(drivers);
+			System.out.println("the driver " + drivers.get(2).getDriverName() + " who traveled maximum distance is "
+					+ maximumTravelledList);
+			break;
+		default:
+			System.out.println("Please Enter Values in mention Order: catagory,Driver Id,Driver Name, Total Distance");
+
+			break;
 		}
-
-		// Getting MaximumDisttance TravelledDriver
-		int maximumTravelledList = Travel.RetriveMaximumDistanceTravelledDriver(noOfDriversList);
-		System.out.println("the driver " + drivers.get(2).getDriverName() + " who traveled maximum distance is"
-				+ maximumTravelledList);
 
 	}
 
